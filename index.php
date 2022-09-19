0c07eaddfc1bb0ccc307df04e0bebccd32daea34
MelissaBarnett <78051622+MelissaBarnett90@users.noreply.github.com>
2022-09-18 22:34:36 -0500

    first Commit

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PP Primer - Index</title>
+</head>
+<body>
+    <!-- Basic HTML -->
+    <h1>Hello World - PHP Primer</h1>
+     
+    <?php
+    //Printing to HMTK using echo
+        echo "Hello PHP!";
+        echo "<br/>";
+        echo "Second Line";
+           
+
+    ?>
+
+    <?php 
+    //declare variable
+        $name = "Melissa Barnett";
+        $age = "24";
+        echo "</br>";
+        //print variable
+        echo $name;
+        echo '<h1>My name is:  '.$name.' </h1>';
+        echo '<h1>My name is:  '.$age.' </h1>';
+        
+        echo "<h1>My Name is: $name </h1>";
+    ?>
+
+
+
+</body>
+</html>
